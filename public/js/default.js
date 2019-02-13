3908e050b7988bcf9a619f27a776c61a249a8371
Christian Decembrana <work.christiandecembrana@gmail.com>
2019-02-13 10:28:37 +0800

    Commited

--- public/js/default.js
+++ public/js/default.js
@@ -18,3 +18,15 @@ function confirmSubmit( msg )
       return false;
     }
 }
+
+function filterizeSearch()
+{
+    $("#filterInput").on("keyup", function() 
+    {
+      var value = $(this).val().toLowerCase();
+      $("#filterTable tr").filter(function() 
+      {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+}

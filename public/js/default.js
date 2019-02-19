d28f6c23984c183cde2c434927819a23eecc03d8
Christian Decembrana <work.christiandecembrana@gmail.com>
2019-02-19 17:44:19 +0800

    new

--- public/js/default.js
+++ public/js/default.js
@@ -30,3 +30,55 @@ function filterizeSearch()
       });
     });
 }
+
+function drawChart() {
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('datetime', 'Time of Day');
+    data.addColumn('number', 'Motivation Level');
+
+    data.addRows([
+      [new Date(2019, 2, 06, 10,01,00,00), 1],    
+      [new Date(2019, 3, 06, 10,05,00,00), 2],    
+      [new Date(2019, 4, 06, 10,07,00,00), 3],    
+      [new Date(2019, 5, 06, 09,01,00,00), 4],    
+      [new Date(2019, 6, 06, 10,11,00,00), 5],    
+    ]);
+
+    var options = {
+      width: 900,
+      height: 500,
+      legend: {position: 'none'},
+      enableInteractivity: false,
+      chartArea: {
+        width: '85%'
+      },
+      hAxis: {
+        viewWindow: {
+          min: new Date(2019, 01, 00),
+          max: new Date(2019, 12, 31)
+        },
+        gridlines: {
+          count: -1,
+          units: {
+            days: {format: ['MMM dd']},
+            hours: {format: ['HH:mm', 'ha']},
+          }
+        },
+        minorGridlines: {
+          units: {
+            hours: {format: ['hh:mm:ss a', 'ha']},
+            minutes: {format: ['HH:mm a Z', ':mm']}
+          }
+        }
+      }
+    };
+
+    var chart = new google.visualization.LineChart(
+      document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+
+    var button = document.getElementById('change');
+    var isChanged = false;
+  }
